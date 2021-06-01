b5770ba876244d5b96492cf78974906e5caf0283
kzkamitani <kz_kmtn@yahoo.co.jp>
2021-06-01 17:40:14 +0900

    first commit

--- mlapp/views.py
+++ mlapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import InputForm
 
-# Create your views here.
+def index(request):
+    return render(request, 'mlapp/index.html')
+
+def input_form(request):
+    form = InputForm()
+    return render(request, 'mlapp/input_form.html', {'form':form})
